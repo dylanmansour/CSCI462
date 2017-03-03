066ca87d0ecba86d2b7a641eeef7afe94d5e36f1
Eric Hofesmann <ehofesmann@gmail.com>
2017-03-02 22:04:51 -0500

    Add Text Box
    Github test

--- Changeover_462/Changeover_462/Form1.cs
+++ Changeover_462/Changeover_462/Form1.cs
@@ -26,5 +26,10 @@ namespace Changeover_462
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("New Station");
+        }
     }
 }
